ed51056a01f1d65e4d716425196da186ca9521fa
Brian Cain <bcain@quicinc.com>
2024-08-27 19:19:43 -0700

    target/hexagon: Implement {c,}swi helpers
    {c,}swi are the "software interrupt"/"Cancel pending interrupts" instructions.
    
    
    Reviewed-by: Taylor Simpson <ltaylorsimpson@gmail.com>
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- target/hexagon/op_helper.c
+++ target/hexagon/op_helper.c
@@ -23,6 +23,7 @@
 #include "exec/helper-proto.h"
 #include "fpu/softfloat.h"
 #include "cpu.h"
+#include "exec/cpu-interrupt.h"
 #include "internal.h"
 #include "macros.h"
 #include "sys_macros.h"
@@ -1443,12 +1444,14 @@ void HELPER(siad)(CPUHexagonState *env, uint32_t mask)
 
 void HELPER(swi)(CPUHexagonState *env, uint32_t mask)
 {
-    g_assert_not_reached();
+    BQL_LOCK_GUARD();
+    hex_raise_interrupts(env, mask, CPU_INTERRUPT_SWI);
 }
 
 void HELPER(cswi)(CPUHexagonState *env, uint32_t mask)
 {
-    g_assert_not_reached();
+    BQL_LOCK_GUARD();
+    hex_clear_interrupts(env, mask, CPU_INTERRUPT_SWI);
 }
 
 void HELPER(iassignw)(CPUHexagonState *env, uint32_t src)
