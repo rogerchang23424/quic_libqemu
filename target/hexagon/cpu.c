a5ceb75133a3fd959efa005bd26211b814c1a6d3
Sid Manning <sidneym@quicinc.com>
2024-12-18 09:23:33 -0800

    hw/hexagon: Add support for cfgbase
    Reviewed-by: Taylor Simpson <ltaylorsimpson@gmail.com>
    Signed-off-by: Sid Manning <sidneym@quicinc.com>

--- target/hexagon/cpu.c
+++ target/hexagon/cpu.c
@@ -390,6 +390,8 @@ void hexagon_cpu_soft_reset(CPUHexagonState *env)
 }
 #endif
 
+
+#define HEXAGON_CFG_ADDR_BASE(addr) (((addr) >> 16) & 0x0fffff)
 static void hexagon_cpu_reset_hold(Object *obj, ResetType type)
 {
     CPUState *cs = CPU(obj);
