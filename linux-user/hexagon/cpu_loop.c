0484110afafd38be21d7f502a17ab52025fa4612
Brian Cain <brian.cain@oss.qualcomm.com>
2025-03-25 20:30:40 -0700

    linux-user/hexagon: add trap1 handling in cpu_loop
    Without an explicit case for HEX_EVENT_TRAP1 the cpu_loop
    falls through to the default handler and aborts.  Add a
    case that reports the trap1 cause before exiting so that
    users get a clear diagnostic.
    
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

--- linux-user/hexagon/cpu_loop.c
+++ linux-user/hexagon/cpu_loop.c
@@ -61,25 +61,28 @@ void cpu_loop(CPUHexagonState *env)
                 env->gpr[0] = ret;
             }
             break;
+        case HEX_EVENT_TRAP1:
+            EXCP_DUMP(env, "\nqemu: trap1 exception 0x%x - aborting\n",
+                     trapnr);
+            exit(EXIT_FAILURE);
         case HEX_EVENT_PRECISE:
             switch (env->cause_code) {
             case HEX_CAUSE_FETCH_NO_UPAGE:
             case HEX_CAUSE_PRIV_NO_UREAD:
             case HEX_CAUSE_PRIV_NO_UWRITE:
-            force_sig_fault(TARGET_SIGSEGV, TARGET_SEGV_MAPERR,
-                    env->gpr[HEX_REG_PC]);
-
-            break;
+                force_sig_fault(TARGET_SIGSEGV, TARGET_SEGV_MAPERR,
+                                env->gpr[HEX_REG_PC]);
+                break;
             case HEX_CAUSE_PRIV_USER_NO_GINSN:
             case HEX_CAUSE_PRIV_USER_NO_SINSN:
             case HEX_CAUSE_INVALID_PACKET:
-            force_sig_fault(TARGET_SIGILL, TARGET_ILL_ILLOPC,
-                    env->gpr[HEX_REG_PC]);
-            break;
+                force_sig_fault(TARGET_SIGILL, TARGET_ILL_ILLOPC,
+                                env->gpr[HEX_REG_PC]);
+                break;
             default:
                 EXCP_DUMP(env, "\nqemu: unhandled CPU precise exception "
-                    "cause code 0x%x - aborting\n",
-                    env->cause_code);
+                          "cause code 0x%x - aborting\n",
+                          env->cause_code);
                 exit(EXIT_FAILURE);
             }
             break;
