3ab90352dac1465787c1b2f5e6f31f8e24208667
Brian Cain <brian.cain@oss.qualcomm.com>
2025-08-19 19:18:34 -0500

    tests/tcg/hexagon: add hello world smoke test
    Provide a minimal linux-user test that verifies basic C
    library functionality works under hexagon linux-user
    emulation.  This serves as a quick smoke test for the
    toolchain and runtime environment.
    
    Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

---
+++ tests/tcg/hexagon/hello.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Hello World\n");
+    return 0;
+}
