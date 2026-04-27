qualcomm/qemu

linux-user/hexagon: Enable HVX_LENGTH_128B and HVX_IEEE_FP HWCAP flags
Enable HWCAP_HEXAGON_HVX_LENGTH_128B unconditionally alongside the
existing HWCAP_HEXAGON_HVX flag, since QEMU always emulates 128-byte
HVX vectors.

Enable HWCAP_HEXAGON_HVX_IEEE_FP for ISA v79 and later, which is
when IEEE floating-point HVX instructions were introduced.

Signed-off-by: Brian Cain <brian.cain@oss.qualcomm.com>

linux-user/hexagon/elfload.c
@@ -74,6 +74,11 @@ abi_ulong get_elf_hwcap(CPUState *cs)
     }
 
     hwcaps |= HWCAP_HEXAGON_HVX;
+    hwcaps |= HWCAP_HEXAGON_HVX_LENGTH_128B;
+
+    if (isa_version >= 0x79) {
+        hwcaps |= HWCAP_HEXAGON_HVX_IEEE_FP;
+    }
 
     return hwcaps;
 }

rogerchang23424/quic_libqemu

linux-user/hexagon/elfload.c
@@ -83,6 +83,11 @@ abi_ulong get_elf_hwcap(CPUState *cs)
     }
 
     hwcaps |= HWCAP_HEXAGON_HVX;
+    hwcaps |= HWCAP_HEXAGON_HVX_LENGTH_128B;
+
+    if (isa_version >= 0x68) {
+        hwcaps |= HWCAP_HEXAGON_HVX_IEEE_FP;
+    }
 
     return hwcaps;
 }